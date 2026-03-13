6c30de9e5b3a46520bc85a3e9a93d2f3aa3aeb88
SnapProject <tokenfuseai@gmail.com>
2026-03-13 20:53:24 +0100

    Update Impressum.tsx

--- src/pages/Impressum.tsx
+++ src/pages/Impressum.tsx
@@ -72,7 +72,7 @@ const Impressum = () => {
               </div>
               
               <p className="text-gray-400 text-xs sm:text-sm">
-                © 2025 SnapFare. Alle Rechte vorbehalten.
+                © 2026 SnapFare. Alle Rechte vorbehalten.
               </p>
             </div>
           </div>
